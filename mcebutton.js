df9957c4558477147d67180b30ebcc4b382c685f
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-15 12:42:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/youtube-embed/trunk@410186 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mcebutton.js
+++
@@ -1,1 +0,0 @@
-(function(){tinymce.create('tinymce.plugins.youtube',{init:function(ed,url){ed.addButton('YouTube',{title:'YouTube Embed',image:url+'/youtube.png',onclick:function(){ed.selection.setContent('[youtube]'+ed.selection.getContent()+'[/youtube]')}})},createControl:function(n,cm){return null},});tinymce.PluginManager.add('YouTube',tinymce.plugins.youtube)})();
