ce36a9d231ed6e44e19d6522609689fa223fba9c
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-23 13:57:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/youtube-embed/trunk@466489 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/about.php
@@ -0,0 +1,62 @@
+<?php
+/**
+* About Page
+*
+* About the plugin
+*
+* @package	YouTubeEmbed
+* @since	2.0
+*/
+?>
+<div class="wrap">
+<div class="icon32"><img src="<?php echo plugins_url(); ?>/youtube-embed/images/screen_icon.png" alt="" title="" height="32px" width="32px"/><br /></div>
+
+<h2><?php _e( 'About Artiss YouTube Embed'); ?></h2>
+
+<?php
+$options = ye_set_general_defaults();
+if ( $options[ 'donation' ] != 1 ) : ?>
+
+<div style="text-align: center;"><script type="text/javascript">
+var psHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + psHost + "pluginsponsors.com/direct/spsn/display.php?client=youtube-embed&spot=' type='text/javascript'%3E%3C/script%3E"));
+</script></div>
+
+<?php endif; ?>
+
+<p><?php _e( 'You are using Artiss YouTube Embed version ' . youtube_embed_version . '. It was written by David Artiss.' ); ?></p>
+
+<a href="http://www.youtube.com/"><img src="<?php echo plugins_url(); ?>/youtube-embed/images/poweredby.png" alt="Powered by YouTube" title="Powered by YouTube" align="right" /></a>
+
+<?php
+
+_e( '<h3>Copyrights</h3>' );
+
+_e( '<p>YouTube, and all associated logos, is the copyright of Google Inc.</p>' );
+
+_e( '<p>EmbedPlus is copyright. Read the <a href="http://embedplus.com/terms.aspx" target="_blank">Terms &amp; Conditions of Use</a>.</p>' );
+
+_e( '<h3>Acknowledgements</h3>' );
+
+_e( '<p>Images have been compressed with <a href="http://www.smushit.com/ysmush.it/">Smush.it</a>.</p>' );
+
+_e( '<p>JavaScript has been compressed with <a href="http://javascriptcompressor.com/">JavaScript Compressor</a>.</p>' );
+
+_e( '<p>YouTube icons are courtesy of <a href="http://icondock.com/">IconDock</a>. Other icons are by <a href="http://p.yusukekamiyamane.com/">Yusuke Kamiyamane</a>.</p>' );
+
+_e( '<h3>Support Information</h3>' );
+
+_e( '<p>Useful support information and links can be found by clicking on the Help tab at the top of each of the Artiss YouTube Embed administration screens.</p>' );
+
+_e( '<h4>This plugin, and all support, is supplied for free, but <a title="Donate" href="http://artiss.co.uk/donate" target="_blank">donations</a> are always welcome.</h4>' );
+
+_e( '<h3>Stay in Touch</h3>' );
+
+_e( '<p><a href="http://www.artiss.co.uk/wp-plugins">See the full list</a> of Artiss plugins, including beta releases.</p>' );
+
+_e( '<p><a href="http://www.twitter.com/artiss_tech">Follow Artiss.co.uk</a> on Twitter.</p>' );
+
+_e( '<p><a href="http://www.artiss.co.uk/feed">Subscribe</a> to the Artiss.co.uk news feed.</p>' );
+
+?>
+</div>
