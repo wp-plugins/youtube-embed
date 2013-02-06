ba99c9ac33b5fb46b1e94d0dff93e4a866cfb4e7
Applian <Applian@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-06 10:28:18 +0000

    Re-branded version 3 release
    git-svn-id: https://plugins.svn.wordpress.org/youtube-embed/tags/3.0@664181 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/generate-download-code.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* Generate Download Code
+*
+* Create code to allow a YouTube video to be downloaded
+*
+* @package	YouTube-Embed
+* @since	2.0
+*
+* @uses		vye_extract_id				Extract an ID from a string
+* @uses		vye_validate_id				Confirm the type of video
+* @uses		vye_error					Display an error
+* @uses		vye_get_file				Fetch a file
+*
+* @param    string	$id					YouTube video ID
+* @return	string						Download HTML
+*/
+
+function vye_generate_download_code( $id ) {
+
+	if ( $id == '' ) { return vye_error( __ ( 'No YouTube ID was found.', 'youtube-embed' ) ); }
+
+	// Extract the ID if a full URL has been specified
+
+	$id = vye_extract_id( $id );
+
+	// Check what type of video it is and whether it's valid
+
+	$embed_type = vye_validate_id( $id );
+	if ( $embed_type != 'v' ) {
+		if ( strlen( $embed_type ) > 1 ) {
+			return vye_error( $embed_type );
+		} else {
+			return vye_error( sprintf( __( 'The YouTube ID of %s is invalid.', 'youtube-embed' ), $id ) );
+		}
+	}
+
+	// See if a ShareASale affiliate ID has been specified - if so, get it!
+
+	$options = vye_set_general_defaults();
+	if ( $options[ 'shareasale_id' ] == '' ) {
+		$affiliate = '';
+	} else {
+		$affiliate = $options[ 'shareasale_id' ];
+	}
+
+	// Get the link from Vixy
+
+	$url = 'http://vixy.net/downloadbar?id=' . $id . '&sip=' . $_SERVER['SERVER_ADDR']. '&affiliate_id=' . $affiliate;
+
+	$file = vye_get_file( $url );
+
+	// Return the resulting code
+
+	return $file[ 'file' ];
+}
+?>
